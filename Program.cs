79de419307641c440633d2e52fdab0590f6a052c
William-italia <williamitalia70@outlook.com>
2021-10-24 06:39:23 -0300

    exercicio Doador de sangue

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ExercicioDoadorDeSangue
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      string idade, peso;
+      int idadeNR, pesoNR;
+      Console.WriteLine("- - - Veja se você pode ou não ser um doador de sangue - - -");
+      Console.WriteLine("---RESULTADO ESTRITAMENTE EDUCACIONAL.");
+      Console.WriteLine($"http://www.prosangue.sp.gov.br/artigos/requisitos_basicos_para_doacao.html\n");
+
+
+      Console.Write("Digite sua idade: ");
+      idade = Console.ReadLine();
+      bool Idadereal = Int32.TryParse(idade, out idadeNR);
+
+      Console.Write("Digite seu peso: ");
+      peso = Console.ReadLine();
+      bool pesoreal = Int32.TryParse(peso, out pesoNR);
+
+      if (!Idadereal || !pesoreal)
+      {
+        Console.WriteLine($"\nDigite um numero valido.");
+        Environment.Exit(-1);
+      }
+      if (idadeNR < 0 || idadeNR > 125)
+      {
+        Console.WriteLine("\nDigite uma idade valida");
+        Environment.Exit(-1);
+      }
+      if (idadeNR < 16 || idadeNR > 69)
+      {
+        Console.WriteLine("\nVocê não pode ser doador de sangue.");
+      }
+
+      if (idadeNR >= 18 && idadeNR <= 69)
+      {
+        if (idadeNR == 69)
+        {
+          Console.WriteLine("\ntudo bem, desde que a primeira doação tenha sido feita até 60 anos.");
+        }
+        if (pesoNR < 50)
+        {
+          Console.WriteLine("\nDoadores tem que ter pelo menos 50kg");
+        }
+        else if (pesoNR >= 50)
+        {
+          Console.WriteLine($"\nVocê pode doar sangue // idade: {idadeNR} peso: {pesoNR}");
+        }
+      }
+      if (idadeNR >= 16 && idadeNR < 18)
+      {
+        if (pesoNR < 50)
+        {
+          Console.WriteLine("\nDoadores tem que ter pelo menos 50kg");
+        }
+        else if (pesoNR >= 50)
+        {
+          Console.WriteLine("\nVocê tem permissão de seus responsaveis? Digite [S] para sim e outro para nao.");
+          bool Permissao = Console.ReadLine().ToUpper() == "S";
+
+          if (!Permissao)
+          {
+            Console.WriteLine("\nEspere mais alguns anos");
+          }
+          else
+          {
+            Console.WriteLine("\nVá com seus responsaveis ao banco de sangue.");
+          }
+
+        }
+
+
+      }
+
+
+
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # ExercicioDoadorDeSangue
-Solicite a idade do usuário. Avalie se ele pode ser doador de sangue (i.e. possui idade entre 18 e 67 anos). Informe o resultado da avaliação.
+_Solicite a idade do usuário. Avalie se ele pode ser doador de sangue (i.e. possui idade entre 18 e 67 anos). Informe o resultado da avaliação._
+## Exemplos. . :
+```
+Digite sua idade: 16 
+Digite seu peso: 50
+
+Você tem permissão de seus responsaveis? Digite [S] para sim e outro para nao.
+S
+
+Vá com seus responsaveis ao banco de sangue.
+```
+OU
+```
+Digite sua idade: 16
+Digite seu peso: 50
+
+Você tem permissão de seus responsaveis? Digite [S] para sim e outro para nao.
+N
+
+Espere mais alguns anos
+```
+OU
+```
+Digite sua idade: 69
+Digite seu peso: 50
+
+tudo bem, desde que a primeira doação tenha sido feita até 60 anos.
+
+Você pode doar sangue // idade: 69 peso: 50
+```
+OU
+```
+Digite sua idade: 25
+Digite seu peso: 50
+
+Você pode doar sangue // idade: 25 peso: 50
+```
+OU
+```
+Digite sua idade: 56
+Digite seu peso: 44
+
+Doadores tem que ter pelo menos 50kg
+```
+OU
+```
+- - - Veja se você pode ou não ser um doador de sangue - - -
+---RESULTADO ESTRITAMENTE EDUCACIONAL.
+http://www.prosangue.sp.gov.br/artigos/requisitos_basicos_para_doacao.html
+
+Digite sua idade: 70
+Digite seu peso: 50
+
+Você não pode ser doador de sangue.
+```
+---
+William Itália // _DevCs_ _Linguagem C#_
